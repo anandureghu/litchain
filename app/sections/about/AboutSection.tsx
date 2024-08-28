21209875291c9818069dde05fc7bc4a757360598
anandureghu <anandukr145@gmail.com>
2024-08-28 12:04:31 +0530

    added assets in about section

--- app/sections/about/AboutSection.tsx
+++ app/sections/about/AboutSection.tsx
@@ -1,3 +1,5 @@
+import { underline } from "@/app/assets";
+import Image from "next/image";
 import React from "react";
 
 const AboutSection = () => {
@@ -6,6 +8,9 @@ const AboutSection = () => {
       <p className="text-9xl text-center text-slate-50">
         Learn about our services
       </p>
+      <div className="w-fit mx-auto mt-5">
+        <Image src={underline} alt="underline" width={600} />
+      </div>
       <p className="text-6xl text-center mt-8 leading-[80px] px-[100px] gradient-text-4 w-[fit-content]">
         LITCHAIN empowering transactions with blockchain technology. <br />{" "}
         Secure, transparent, and efficient. <br />
